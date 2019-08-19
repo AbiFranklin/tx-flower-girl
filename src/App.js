55ce07b2ec9feb8ee42b872e9bb8a11db7af2424
Abigayle Franklin <Abifranklin@gmail.com>
2019-08-19 09:52:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
         "message": this.state.message
     }
     
-    axios.post('https://tx-flower-girl-node-api-dauqz7oj8.now.sh/', data)
+    axios.post(https://tx-flower-girl-node-api-dauqz7oj8.now.sh/, data)
     .then( res => {
         this.setState({ sent: true }, this.resetForm())
     })
