8238a0a3bbb42feeff2af1e2f4f3aabde458a454
Abigayle Franklin <Abifranklin@gmail.com>
2019-08-19 12:18:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -61,13 +61,13 @@ class App extends React.Component {
           <Col>
           <Form.Group controlId="formBasicName">
             <Form.Label>Name</Form.Label>
-            <Form.Control placeholder="Name" onChange={e => this.setState({ name: e.target.value })}/>
+            <Form.Control placeholder="Name" onChange={e => this.setState({ name: e.target.value })} onSubmit={e => e.target.value = ''} />
           </Form.Group>
           </Col>
           <Col>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" onChange={e => this.setState({ email: e.target.value })}/>
+            <Form.Control type="email" placeholder="Enter email" onChange={e => this.setState({ email: e.target.value })} onSubmit={e => e.target.value = ''} />
           </Form.Group>
           </Col>
           </Form.Row>
@@ -75,7 +75,7 @@ class App extends React.Component {
               We'll never share your email with anyone else.
             </Form.Text>
           <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Control as="textarea" rows="3" placeholder='Message' onChange={e => this.setState({ message: e.target.value })}/>
+          <Form.Control as="textarea" rows="3" placeholder='Message' onChange={e => this.setState({ message: e.target.value })} onSubmit={e => e.target.value = ''} />
         </Form.Group>
           <Button variant={this.state.buttonvar} type="submit">
             {this.state.buttontext}
