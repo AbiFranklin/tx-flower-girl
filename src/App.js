b0cccc35513ba2aff554b165a1e03f87a37967a3
Abigayle Franklin <abifanklin@gmail.com>
2019-08-24 18:22:53 -0500

    Menu and font implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import ContactForm from '../src/Components/Form'
+import Menu from '../src/Components/Menu'
 
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Menu />
         <ContactForm />
       </div>
     );
